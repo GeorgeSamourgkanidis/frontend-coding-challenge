c0d048a0de63d80fe10d9d9482be9b53aa316392
GeorgeSamourgkanidis <giorgossamou1@gmail.com>
2022-11-14 00:21:50 +0200

    get Stores data and implement unfinished cards

--- book-store-frontend/src/components/Stores/StoreItem/StoreItem.jsx
+++ book-store-frontend/src/components/Stores/StoreItem/StoreItem.jsx
@@ -1,9 +1,22 @@
 import "./StoreItem.scss";
 import BooksTable from "./BooksTable/BooksTable";
 
-const StoreItem = () => {
+const StoreItem = (props) => {
+  const { storeAttributes, books, countryId } = props;
+
   return (
-    <div>
+    <div className="storeContainer">
+      <div className="content">
+        <img className="image" src={storeAttributes.storeImage} alt="" />
+        <div className="details">
+          <h2 className="title">{storeAttributes.name}</h2>
+        </div>
+      </div>
+
+      <div className="footer">
+        <span>{storeAttributes.website}</span>
+        <span>flag</span>
+      </div>
       <BooksTable />
     </div>
   );
--- book-store-frontend/src/components/Stores/Stores.jsx
+++ book-store-frontend/src/components/Stores/Stores.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
 import "./Stores.scss";
 import StoreItem from "./StoreItem/StoreItem";
 
 const Stores = () => {
+  const [storesData, setStoresData] = useState([]);
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL + "/stores")
+      .then((response) => response.json())
+      .then((res) => {
+        setStoresData(res.data);
+        console.log(res.data);
+      });
+  }, []);
+
   return (
-    <div>
-        <StoreItem />
+    <div className="storesListContainer">
+      {storesData.length > 0 &&
+        storesData.map((storeData, storeIndex) => {
+          return (
+            <StoreItem
+              key={"store" + storeIndex}
+              storeAttributes={storeData.attributes}
+              books={
+                storeData.relationships.books
+                  ? storeData.relationships.books.data
+                  : []
+              }
+              countryId={storeData.relationships.countries.data.id}
+            />
+          );
+        })}
     </div>
   );
 };
